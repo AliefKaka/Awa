dc3d6c2dfeed58dc47d4cc48985bf3bf48383615
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 00:46:20 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 // components/Footer.js
 export default function Footer() {
   return (
-    <footer id="footer" className="bg-background text-center py-8 text-gray-500">
-      <p>&copy; 2025 ABSOLUTE OPBR Alliance. All rights reserved.</p>
+    <footer id="footer" className="text-center py-10 text-gray-500">
+      &copy; 2025 ABSOLUTE OPBR Alliance. All rights reserved.
     </footer>
   );
 }
