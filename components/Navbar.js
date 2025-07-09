3723c2c347bfd770aec2284d47cc2df50c64cb12
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 00:43:40 +0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 w-full z-50 bg-black/30 backdrop-blur-md py-4 px-6 flex justify-between items-center">
-      <h1 className="text-primary font-bold text-xl">ABSOLUTE</h1>
-      <div className="space-x-6">
-        <Link href="#hero" className="text-white hover:text-primary transition">Home</Link>
-        <Link href="#features" className="text-white hover:text-primary transition">Fitur</Link>
-        <Link href="#footer" className="text-white hover:text-primary transition">Kontak</Link>
+    <nav className="fixed w-full top-0 left-0 px-8 py-4 bg-black/20 backdrop-blur-md flex justify-between items-center z-50">
+      <h1 className="text-cyan-400 font-semibold text-xl">ABSOLUTE</h1>
+      <div className="space-x-4">
+        <a href="#hero" className="text-white hover:text-cyan-400 transition">Home</a>
+        <a href="#features" className="text-white hover:text-cyan-400 transition">Fitur</a>
+        <a href="#footer" className="text-white hover:text-cyan-400 transition">Kontak</a>
       </div>
     </nav>
   );
