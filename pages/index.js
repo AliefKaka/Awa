48ad5aaf91269e97adfcc949cc060c936c70b551
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 00:28:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 // pages/index.js
 import Head from 'next/head';
-import HeroSection from '@/components/HeroSection';
-import Features from '@/components/Features';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import HeroSection from '../components/HeroSection';
+import Features from '../components/Features';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
