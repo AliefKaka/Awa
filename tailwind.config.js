90318b3e471ec008e361de9486e9fbb16f6ac5eb
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 00:15:47 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00d4ff',
+        background: '#0c0f1a'
+      },
+    },
+  },
+  plugins: [],
+};
