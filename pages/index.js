f829089d5702e4b11ef53a7cd42b454961aa7da4
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 00:11:54 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+// pages/index.js
+import Head from 'next/head';
+import HeroSection from '@/components/HeroSection';
+import Features from '@/components/Features';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ABSOLUTE OPBR Alliance</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Navbar />
+      <HeroSection />
+      <Features />
+      <Footer />
+    </>
+  );
+}
