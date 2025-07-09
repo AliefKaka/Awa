1902ff012241aa006e4a10fe4159bc106543bab3
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 01:38:43 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import Divisions from '../components/Divisions'
 import Rules from '../components/Rules'
 import RegisterForm from '../components/RegisterForm'
+import SwipeCards from '../components/SwipeCards'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Divisions />
       <Rules />
       <RegisterForm />
+      <SwipeCards />
     </>
   )
 }
