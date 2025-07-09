1f493952fe8bc4703bf9dc2607c9010378d662e1
AliefKaka <aliefkaka123@gmail.com>
2025-07-10 01:21:05 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 // pages/_app.js
-import '../styles/globals.css';
+import '../styles/globals.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true })
+  }, [])
+  return <Component {...pageProps} />
 }
